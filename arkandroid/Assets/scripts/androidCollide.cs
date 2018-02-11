86e6e027e82305477f1675c57561595cf1aa08ef
aledebarba <ale@alemacedo.com>
2018-02-11 13:11:54 -0200

    Jogo pronto - ainda sem suporte mobile.

--- arkandroid/Assets/scripts/androidCollide.cs
+++ arkandroid/Assets/scripts/androidCollide.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class androidCollide : MonoBehaviour {
 
-	void OnCollisionEnter2D(Collision2D c) {
-		Destroy (gameObject);
+	static int androids;
 
+	void Start () {
+		androids = 12;
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		// Destroi os androids e reduz o contador
+		Destroy (gameObject);
+		androids--;
+		// quando o contador zerar, carrega endgame
+		if (androids <= 0) {
+			SceneManager.LoadScene ("endgame");
+
+		}
 	}
 }
